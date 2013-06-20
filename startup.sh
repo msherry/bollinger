e276320d2527d82e878d748f9d7e8f26f6ac122c
Marc Sherry <msherry@gmail.com>
2013-06-20 13:25:03 -0700

    full path

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 # Edit this file and change this next line to your own email address:
 #
 
-cd bollinger
+cd /home/ubuntu/bollinger
 git pull
 ./driver.sh
 
