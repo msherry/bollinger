a466e89462c50b401c82d80acf19d8db6a4d4ef7
Marc Sherry <msherry@gmail.com>
2013-06-24 17:48:15 -0700

    proper mime types -- visible on iphone mail

--- driver.sh
+++ driver.sh
@@ -8,11 +8,16 @@ tail -1 out.txt | grep -q -E ", (\+|-)"
 if [[ ( $? == 0 ) ]];
 then
     # Send status email
-    (cat <<EOF; uuencode output.png output.png ) | /usr/sbin/sendmail -oi -t -f $EMAIL
+    (cat <<EOF; base64 output.png ) | /usr/sbin/sendmail -oi -t -f $EMAIL
 From: $EMAIL
 To: $EMAIL
 Subject: Here is a graph
+MIME-Version: 1.0
+Content-Type: multipart/mixed; boundary=_frontier
 
+--_frontier
+Content-Type: image/png
+Content-Transfer-Encoding: base64
 EOF
 else
     (cat <<EOF; tail -1 out.txt) | /usr/sbin/sendmail -oi -t -f $EMAIL
